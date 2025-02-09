b8211cbe78c4771755ab2d7571056179cbd39dcf
Valentina Belosludtseva <valbelos@yandex.ru>
2025-02-09 15:45:27 +0300

    processing init

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from datetime import datetime
 from src.widget import get_date, mask_account_card
+from src.processing import filter_by_state
 
-simple = open("data/card_account.txt", encoding="utf-8")
+
+simple = open("data/card_account.txt", encoding = "utf-8")
 for line in simple:
     print(mask_account_card(line))
 
 now_date_time = str(datetime.now())
 now_date = get_date(now_date_time)
 print(now_date)
+
+state_now = open("Data/states.txt", encoding ="utf-8")
+for line in state_now:
+    print(import filter_by_state(line))
---
+++ src/processing.py
@@ -0,0 +1,9 @@
+def filter_by_state(loc_dict: dict, state = 'EXECUTED') -> list[dict]:
+    new_list = []
+    another_list = []
+    if 'state':
+        new_list = new_list.append(loc_dict)
+    else:
+        another_list = another_list.append(loc_dict)
+
+    return new_list, another_list
