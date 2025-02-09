e35ea92d240113728cb5bab66b8d09efb6ade0e7
Valentina Belosludtseva <valbelos@yandex.ru>
2025-02-10 00:23:07 +0300

    processing delevop

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from datetime import datetime
-from src.widget import get_date, mask_account_card
+
 from src.processing import filter_by_state
+from src.widget import get_date, mask_account_card
 
-
-simple = open("data/card_account.txt", encoding = "utf-8")
+simple = open("data/card_account.txt", encoding="utf-8")
 for line in simple:
     print(mask_account_card(line))
 
@@ -11,6 +11,10 @@ now_date_time = str(datetime.now())
 now_date = get_date(now_date_time)
 print(now_date)
 
-state_now = open("Data/states.txt", encoding ="utf-8")
+state_now = open("data/states.txt", encoding="utf-8")
+now_state = []
 for line in state_now:
-    print(import filter_by_state(line))
+    line_now = {line}
+    now_state.append(line_now)
+state = filter_by_state(now_state)
+print(state)
--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,9 @@
-def filter_by_state(loc_dict: dict, state = 'EXECUTED') -> list[dict]:
-    new_list = []
-    another_list = []
-    if 'state':
-        new_list = new_list.append(loc_dict)
-    else:
-        another_list = another_list.append(loc_dict)
-
-    return new_list, another_list
+def filter_by_state(data_state: list[dict], state = "EXECUTED") -> tuple[list[dict], list[dict]]:
+    new_list_1 = []
+    new_list_2 = []
+    for i in data_state:
+        if state == True:
+            new_list_1 += i
+        else:
+            new_list_2 += i
+    return f'{new_list_1}{new_list_2}'
