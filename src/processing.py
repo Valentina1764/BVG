4d26c18e9e8fbcb762108616734d1530b05bd891
Valentina Belosludtseva <valbelos@yandex.ru>
2025-02-10 23:01:12 +0300

    fixing def in processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,15 @@
-def filter_by_state(data_state: list[dict], state = "EXECUTED") -> tuple[list[dict], list[dict]]:
-    new_list_1 = []
-    new_list_2 = []
+def filter_by_state(data_state: list, state="EXECUTED") -> list:
+    '''Функция возвращает список словарей в зависимости от значения ключа "state"'''
+    new_list = []
     for i in data_state:
-        if state == True:
-            new_list_1 += i
+        if i["state"] == state:
+            new_list.append(i)
         else:
-            new_list_2 += i
-    return f'{new_list_1}{new_list_2}'
+            continue
+    return new_list
+
+
+def sort_by_date(sort_list: list, rever: bool = False) -> list:
+    '''Функция возвращает отсортированный список по ключу "date"'''
+    sorted_list = sorted(sort_list, key=lambda x: x["date"], reverse=rever)
+    return sorted_list
