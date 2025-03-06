739567d2ec701a1806fc5494b220f0800ec49dd0
Valentina Belosludtseva <valbelos@yandex.ru>
2025-03-06 21:21:41 +0300

    Add decorator and test decorator

---
+++ tests/test_my_decorators.py
@@ -0,0 +1,33 @@
+import os
+
+import pytest
+
+from src.my_decorators import log
+
+
+def test_error_handling_in_function(capsys):
+    """Тест обработки ошибки в функции с логированием в консоль."""
+
+    @log()
+    def divide(a, b):
+        return a / b
+
+
+def test_logging_to_file(tmpdir):
+    """Тест логирования в файл."""
+    log_filename = os.path.join(tmpdir, "log.txt")
+
+    @log(log_filename)
+    def multiply(a, b):
+        return a * b
+
+    result = multiply(3, 4)
+    assert result == 12
+
+    with open(log_filename, "r") as file:
+        logs = file.read()
+        expected_logs = (
+            "INFO - Функция 'multiply' выполнена успешно; Аргументы: (3, 4), {}; "
+            "Результат: 12; Время выполнения: 0.00 сек"
+        )
+        assert logs.strip() == expected_logs
